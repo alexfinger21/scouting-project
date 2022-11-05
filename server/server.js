d9b33afab6e2b625aae6245ad6456c9af2b03edd
alexfinger21 <cdx9933@gmail.com>
2022-11-05 16:46:43 -0400

    log in page

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const http = require("http")
 const express = require("express")
 const path = require("path")
-
 const app = express()
+
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res) {
+    
+})
