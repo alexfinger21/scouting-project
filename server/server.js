545f35c2fcb6e0d472460a8f373d0dbf59c8dda3
alexfinger21 <cdx9933@gmail.com>
2022-11-09 19:14:16 -0500

    d

--- server/server.js
+++ server/server.js
@@ -2,9 +2,19 @@ const http = require("http")
 const express = require("express")
 const path = require("path")
 const app = express()
+const ejs = require("ejs")
+//app.set("view engine", "ejs")
 
-app.set("view engine", "ejs")
+app.use('/static', express.static("./client/static"));
+
+
+app.engine('html', ejs.renderFile);
+
+app.set("views", "./client")
+app.set('view engine', 'html');
 
 app.get("/", function(req, res) {
-    
+    res.render("login")
 })
+
+app.listen(3000)
