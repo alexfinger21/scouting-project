fd5944ebe26236d6df02755df99b80a7a7656237
BuildTools <unconfigured@null.spigotmc.org>
2022-11-23 19:45:39 -0500

    good job today

--- server/server.js
+++ server/server.js
@@ -10,7 +10,18 @@ app.use("/static", express.static("./client/static"));
 app.set("views", "./client/templates")
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
+//middleware for anyone on the site, checking whether they're logged in or ont
+app.use((req, res, next) => {
+    //TO DO if logged in then render the first game page, otherwise redirect to login
+    console.log(req.path != "/login")
+    if (req.path != "/login") {
+        res.redirect("/login")
+    } else {
+        next()
+    }
+})
+
+app.get("/login", function(req, res) {
     res.render("login")
 })
 
