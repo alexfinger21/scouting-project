749125737065ff6d791acfb6c888769f4ace8fdb
BuildTools <unconfigured@null.spigotmc.org>
2022-11-25 12:36:30 -0500

    Co-authored-by: igor023 <igor024@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const ejs = require("ejs")
+const testUser = {username: "alex", password: "npc"}
 //app.set("view engine", "ejs")
 
 app.use("/static", express.static("./client/static"));
@@ -14,7 +15,7 @@ app.set("view engine", "ejs");
 app.use((req, res, next) => {
     //TO DO if logged in then render the first game page, otherwise redirect to login
     console.log(req.path != "/login")
-    if (req.path != "/login") {
+    if (req.path != "/login" && req.path != "/data-collection") { //for testing purposes we also include data collection
         res.redirect("/login")
     } else {
         next()
@@ -23,6 +24,14 @@ app.use((req, res, next) => {
 
 app.get("/login", function(req, res) {
     res.render("login")
+})
+
+app.get("/data-collection", function(req, res) {
+    res.render("data-collection")
+})
+
+app.post("/login", function(req, res) {
+    //req.params
 })
 
 app.listen(3000)
