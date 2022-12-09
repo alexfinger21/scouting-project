113b62bb280b164a3f4aed67e14b82706646ac11
alexfinger21 <cdx9933@gmail.com>
2022-12-08 21:01:05 -0500

    good job teammates

--- client/static/scripts/data-collection.js
+++ client/static/scripts/data-collection.js
@@ -1,7 +1,29 @@
+import {clamp} from "./utility.js"
+
 window.addEventListener("load", () => {
     const buttonContainers = document.getElementsByClassName("NumberButtonContainer")
 
     for (const container of buttonContainers) {
-        console.log(container.children)
+        for (const child of container.children) {
+            if (child.tagName.toLowerCase() == "input") {
+                //numerical value
+                const parent = child.parentElement
+
+                let plusButton = parent.getElementsByTagName("button")[0]
+                let subtractButton = parent.getElementsByTagName("button")[1]
+                let input = parent.getElementsByTagName("input")[0]
+
+                const inputMin = Number(input.min)
+                const inputMax = Number(input.max) 
+
+                plusButton.addEventListener("click", (event) => {
+                    input.value = clamp(Number(input.value) + 1, inputMin, inputMax)
+                })
+
+                subtractButton.addEventListener("click", (event) => {
+                    input.value = clamp(Number(input.value) - 1, inputMin, inputMax)
+                })
+            }
+        }
     }
 })
