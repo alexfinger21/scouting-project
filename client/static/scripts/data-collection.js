db791e0f94a16f2a97885fd3b27f99b349a4744a
Igor96 <thedarthdot@gmail.com>
2022-12-09 10:05:34 -0500

    sws

--- client/static/scripts/data-collection.js
+++ client/static/scripts/data-collection.js
@@ -8,21 +8,37 @@ window.addEventListener("load", () => {
             if (child.tagName.toLowerCase() == "input") {
                 //numerical value
                 const parent = child.parentElement
-
-                let plusButton = parent.getElementsByTagName("button")[0]
-                let subtractButton = parent.getElementsByTagName("button")[1]
                 let input = parent.getElementsByTagName("input")[0]
 
-                const inputMin = Number(input.min)
-                const inputMax = Number(input.max) 
+                if (input.type == "number") {
+                    let plusButton = parent.getElementsByTagName("button")[0]
+                    let subtractButton = parent.getElementsByTagName("button")[1]
+    
+                    const inputMin = Number(input.min)
+                    const inputMax = Number(input.max) 
+    
+                    plusButton.addEventListener("click", (event) => {
+                        input.value = clamp(Number(input.value) + 1, inputMin, inputMax)
+                    })
+    
+                    subtractButton.addEventListener("click", (event) => {
+                        input.value = clamp(Number(input.value) - 1, inputMin, inputMax)
+                    })
+                }
+                else if (input.type == "checkbox") {
+                    let xButton = parent.getElementsByTagName("button")[0]
+                    let checkButton = parent.getElementsByTagName("button")[1]
 
-                plusButton.addEventListener("click", (event) => {
-                    input.value = clamp(Number(input.value) + 1, inputMin, inputMax)
-                })
-
-                subtractButton.addEventListener("click", (event) => {
-                    input.value = clamp(Number(input.value) - 1, inputMin, inputMax)
-                })
+                    xButton.addEventListener("click", (event) => {
+                        xButton.style.backgroundColor = "#3492EA";
+                        checkButton.style.backgroundColor = "D9D9D9";
+                    })
+    
+                    checkButton.addEventListener("click", (event) => {
+                        checkButton.style.backgroundColor = "#3492EA";
+                        xButton.style.backgroundColor = "D9D9D9";
+                    })
+                }
             }
         }
     }
