bf6a23d4981e9541a856642485d267650baec007
Igor024 <thedarthdot@gmail.com>
2022-12-11 15:15:51 -0500

    working on Team Summary

---
+++ client/static/scripts/temp-disabled.js
@@ -0,0 +1,53 @@
+import {clamp} from "./utility.js"
+
+window.addEventListener("load", () => {
+    const buttonContainers = document.getElementsByClassName("NumberButtonContainer")
+
+    for (const container of buttonContainers) {
+        for (const child of container.children) {
+            if (child.tagName.toLowerCase() == "input") {
+                //numerical value
+                const parent = child.parentElement
+                let input = parent.getElementsByTagName("input")[0]
+
+                if (input.type == "number") {
+                    const plusButton = parent.getElementsByTagName("button")[0]
+                    const subtractButton = parent.getElementsByTagName("button")[1]
+    
+                    const inputMin = Number(input.min)
+                    const inputMax = Number(input.max) 
+    
+                    plusButton.addEventListener("click", (event) => {
+                        input.value = clamp(Number(input.value) + 1, inputMin, inputMax)
+                    })
+    
+                    subtractButton.addEventListener("click", (event) => {
+                        input.value = clamp(Number(input.value) - 1, inputMin, inputMax)
+                    })
+                }
+            } else if (child.getElementsByTagName("img").length == 1 && child.tagName.toLowerCase() == "button") {
+                //img
+
+                const parent = child.parentElement
+                
+                const xButton = parent.getElementsByTagName("button")[0]
+                const checkButton = parent.getElementsByTagName("button")[1]
+                
+                //initialize
+
+                xButton.style.backgroundColor = "#D9D9D9"
+                checkButton.style.backgroundColor = "#D9D9D9"
+
+                xButton.addEventListener("click", (event) => {
+                    xButton.style.backgroundColor = "#3492EA"
+                    checkButton.style.backgroundColor = "#D9D9D9"
+                })
+
+                checkButton.addEventListener("click", (event) => {
+                    checkButton.style.backgroundColor = "#3492EA"
+                    xButton.style.backgroundColor = "#D9D9D9"
+                })
+            }
+        }
+    }
+})
