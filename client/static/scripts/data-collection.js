deec057a137a9888b11e7fb19b8aac0d737b47fe
alexfinger21 <cdx9933@gmail.com>
2022-12-17 11:03:21 -0500

    yes

--- client/static/scripts/data-collection.js
+++ client/static/scripts/data-collection.js
@@ -1,16 +1,18 @@
 import {clamp, dataCollectionPath} from "./utility.js"
 
 const observer = new MutationObserver(function(mutations_list) {
-	mutations_list.forEach(function(mutation) {
-		mutation.removedNodes.forEach(function(removed_node) {
-			if(removed_node.id == 'page-holder') {
-				main()
+    mutations_list.forEach(function(mutation) {
+        mutation.removedNodes.forEach(function(removed_node) {
+            if(removed_node.id == 'page-holder') {
+                main()
 			}
 		});
 	});
 });
 
 observer.observe(document.body, { subtree: false, childList: true });
+
+window.addEventListener("load", main)
 
 function main() {
     const buttonContainers = document.getElementsByClassName("NumberButtonContainer")
@@ -106,5 +108,3 @@ function main() {
         }
     }
 }
-
-window.addEventListener("load", main)
