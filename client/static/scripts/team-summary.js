866624c9aff4e06ca5ee141a043344a84b49c8a9
Igor024 <thedarthdot@gmail.com>
2022-12-18 15:41:52 -0500

    Changed code layout, added a graph update button

--- client/static/scripts/team-summary.js
+++ client/static/scripts/team-summary.js
@@ -1,137 +1,52 @@
-import {clamp, dataCollectionPath} from "./utility.js"
+import { generatePoint, createGraph } from "./graphHandler.js";
 
-const observer = new MutationObserver(function(mutations_list) {
-	mutations_list.forEach(function(mutation) {
-		mutation.removedNodes.forEach(function(removed_node) {
-			if(removed_node.id == 'page-holder') {
-				main()
-			}
-		});
-	});
+//When teamsummary is loaded, call the main function 
+const observer = new MutationObserver(function (mutations_list) {
+    mutations_list.forEach(function (mutation) {
+        mutation.removedNodes.forEach(function (removed_node) {
+            if (removed_node.id == 'page-holder') {
+                main()
+            }
+        });
+    });
 });
 
 observer.observe(document.body, { subtree: false, childList: true });
 window.addEventListener("load", main)
 
 function main() {
-    const letterOptions = [
-        "l",
-        "t"
-    ]
-    
-    letterOptions[2]
-    
-    const colorOptions = [
-        "red",
-        "green",
-        "yellow"
-    ]
-    
-    function selectRandom(obj)
-    {
-        let num =  obj[Math.round(Math.random() * (obj.length - 1))]
-        return num
-    }
-    
-    function getColor(color)
-    {
-        if(color == "red")
-        {
-            return "rgb(255,0,0)"
-        }
-        else if(color == "yellow")
-        {
-            return "rgb(255,245,0)"
-        }
-        else
-        {
-            return "rgb(0,255,0)"
-        }
-    }
-    
-    
-    function generatePoint() {
-        const img = new Image()
-    
-        let letter = selectRandom(letterOptions)
-        let color = selectRandom(colorOptions)
-    
-        img.src = "../static/images/letters/" + letter + "/" + color + ".png"
-        console.log(img.src)
-        img.height = 15
-        img.width = 12
-    
-        let teamName = "Team " +  Math.round(Math.random() * 9999)
-
-        return {teamName, x: Math.round(Math.random() * 50), y: Math.round(Math.random() * 50), shape: img, color: getColor(color) }
-    }
-    
+    //create a point array
     let points = []
-    for(let i = 0; i < 10; i++)
-    {
+    for (let i = 0; i < 10; i++) {
         points.push(generatePoint())
     }
-    
-    const ctx = document.getElementById('teamSummaryChart').getContext('2d')
-    let scatterChart = new Chart(ctx, {
-        type: 'scatter',
-        data: {
-            teamName: points.map(p => p.teamName),
-            datasets: [{
-                label: 'Legend',
-                pointRadius: 4,
-                pointStyle: points.map(p => p.shape),
-                borderColor: points.map(p => p.color),
-                pointBackgroundColor: points.map(p => p.color),
-                data: points
-            }]
-        },
-        options: {
-            tooltips: {
-                bodyFontStyle: "bold",
-                footerFontStyle: "normal",
-                callbacks: {
-                    label: function (tooltipItem, data) {
-                        let teamName = data.teamName[tooltipItem.index];
-                        let text = [teamName + ": (" + tooltipItem.xLabel + ', ' + tooltipItem.yLabel + ")"]
-                        return text;
-                    },
-                    //color does not appear before the footer
-                    footer: function (tooltipItems, data) {
-                        return [
-                            "Plays Defense: ✅",
-                            "Avg. Auto Score: 4",
-                            "Avg. Tele-op Score: 2",
-                            "Avg. Endgame Score: 15"
-                        ]
-                    }
-                }
-            },
-            scales: {
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Team Ranking",
-                    },
-                }],
-    
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Average Total Score",
-                    }
-                }],
-            },
-            legend: {
-                display: false
-            },
-            layout: {
-                padding: {
-                    top: 20,
-                    right: 20
-                }
-            }
+
+    //write point array to a scatterplot
+    const ctx = document.getElementById("teamSummaryChart").getContext("2d")
+    let scatterChart = new Chart(ctx, createGraph(points))
+
+    //when the update button is clicked, redraw the graph
+    const updateButton = document.getElementById("update-graph")
+
+    updateButton.addEventListener("click", () => {
+        //animate the button click effect
+        updateButton.style.backgroundColor = "#3b86cc"
+        updateButton.style.boxShadow = "0 2px #1c3750"
+        updateButton.style.transform = "translateY(4px)"
+
+        //create a new point array
+        points = []
+        for (let i = 0; i < 10; i++) {
+            points.push(generatePoint())
         }
+        scatterChart = new Chart(ctx, createGraph(points))
+
+        //animate the button back
+        setTimeout(() => {
+            updateButton.style.backgroundColor = "#3492EA"
+            updateButton.style.boxShadow = "0 6px #3077b9"
+            updateButton.style.transform = ""
+        }, 100); //in milliseconds
     })
 }
 
--- client/static/scripts/utility.js
+++ client/static/scripts/utility.js
@@ -3,4 +3,28 @@ const loginPath = "/login"
 
 const clamp = (num, min, max) => Math.min(Math.max(min, num), max)
 
-export {clamp, dataCollectionPath, loginPath}
+//selects a random value from an array
+function selectRandom(obj)
+{
+    let num =  obj[Math.round(Math.random() * (obj.length - 1))]
+    return num
+}
+
+//returns the rgb value given a color
+function getColor(color)
+{
+    if(color == "red")
+    {
+        return "rgb(255,0,0)"
+    }
+    else if(color == "yellow")
+    {
+        return "rgb(255,245,0)"
+    }
+    else
+    {
+        return "rgb(0,255,0)"
+    }
+}
+
+export {clamp, dataCollectionPath, loginPath, selectRandom, getColor}
