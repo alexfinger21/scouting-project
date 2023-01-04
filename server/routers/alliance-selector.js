377ee32cde0d828d8b55e5f6afa7e279d423137d
BuildTools <unconfigured@null.spigotmc.org>
2023-01-04 13:04:05 -0500

    new main url

--- server/routers/alliance-selector.js
+++ server/routers/alliance-selector.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const router = express.Router()
 
+const user = { //TEST USER
+    team_number: 695,
+    username: "alex",
+    password: "npc",
+    admin: true,
+}
+
 const teams = { //TEST TEAMS
     
 }
 
 router.get("/",  function(req, res) { //only gets used if the url == alliance-selector
     res.render("alliance-selector", {
-        teams
+        teams: teams, user: user
     })
 })
 
