dc754387fbc7cd66a210ffecd3b1c27055cfe2c6
Igor024 <thedarthdot@gmail.com>
2023-01-06 22:49:25 -0500

    gar

---
+++ client/static/scripts/alliance-selector.js
@@ -0,0 +1,35 @@
+console.log("WSP BBG \n\n\n")
+
+//When teamsummary is loaded, call the main function 
+const observer = new MutationObserver(function (mutations_list) {
+    mutations_list.forEach(function (mutation) {
+        mutation.removedNodes.forEach(function (removed_node) {
+            if (removed_node.id == 'page-holder') {
+                main()
+            }
+        })
+    })
+})
+
+observer.observe(document.body, { subtree: false, childList: true });
+window.addEventListener("load", main)
+
+function main() {
+    //when a team buton is clicked, make it empty
+    console.log("GRRRR \n \n \n \n")
+    const divs = document.getElementsByClassName("selected-team")
+
+    for (let i = 0; i < divs.length; i++)
+    {
+        const wrapper = divs[i]
+        const button = wrapper.getElementsByTagName("button")[0]
+
+        button.addEventListener("click", () => {
+            //change to empty class
+            wrapper.classList.replace("selected-team", "empty-team")
+            //change text content
+            button.textContent = "Empty"
+        })
+    }
+
+}
