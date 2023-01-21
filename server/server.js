5bb81ec7c79e815e4dbd236af920bcecca2d349c
alexfinger21 <cdx9933@gmail.com>
2023-01-21 14:29:47 -0500

    d

--- server/server.js
+++ server/server.js
@@ -58,6 +58,7 @@ app.use((req, res, next) => { //if you don't provide a path, app.use will run be
                 const result = results[0]
                 next() //goes to the next middleware function (login or data collection)
             } else {
+                console.log("redirect")
                 res.redirect("/login")
             }
         })
