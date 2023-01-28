1e97723fb63daa9db16e5a6f7dd5aaba81a7ce58
alexfinger21 <cdx9933@gmail.com>
2023-01-28 12:10:39 -0500

    try

--- server/routers/match-listing.js
+++ server/routers/match-listing.js
@@ -9,11 +9,18 @@ const teams = { //TEST TEAMS
     
 }
 
+function addZero(num) {
+    return num<10 ? "0" + num : num
+}
+
 router.get("/",  function(req, res) {
     database.query(`SELECT * FROM game_matchup gm 
     WHERE gm.frc_season_master_sm_year = ` + YEAR + `
     ORDER BY gm_number ASC;`, (err, results) => {
-        console.log(new Date(results[0].gm_timestamp).getMinutes())
+        const date = new Date(results[0].gm_timestamp)
+        const h = addZero(date.getHours())
+        const m = addZero(date.getMinutes())
+        teams.time = h + ":" + m
         res.render("match-listing", {
             teams: results, user: user
         })
