510f06d206b6f40efe874cdb399ca0466cbb09d7
Igor96 <thedarthdot@gmail.com>
2023-01-29 11:21:28 -0500

    hi

--- server/utility.js
+++ server/utility.js
@@ -1,3 +1,5 @@
+const database = require("../database/database.js")
+
 function checkAdmin() {
     return new Promise((resolve) => {
         database.query("SELECT * FROM user_master um WHERE um.um_id = '" + body.username + "' AND team_master_tm_number = " + body.team_number + ";", function (error, results) {
