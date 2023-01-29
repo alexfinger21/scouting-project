5058e39b1284f7f6ea7e2edd6833a631be2a0d33
Igor96 <thedarthdot@gmail.com>
2023-01-29 11:16:36 -0500

    dw

---
+++ server/utility.js
@@ -0,0 +1,15 @@
+function checkAdmin() {
+    return new Promise((resolve) => {
+        database.query("SELECT * FROM user_master um WHERE um.um_id = '" + body.username + "' AND team_master_tm_number = " + body.team_number + ";", function (error, results) {
+            if (error)
+                throw error;
+
+            if (result.um_admin_f == true) {
+                resolve(true)
+            }
+        })
+        resolve(false)
+    })
+}
+
+module.exports = {checkAdmin}
