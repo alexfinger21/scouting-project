43ba272780ae33ecfff5643da56a925ea060979c
Igor96 <thedarthdot@gmail.com>
2023-02-02 20:07:17 -0500

    hs

--- server/routers/data-collection.js
+++ server/routers/data-collection.js
@@ -1,4 +1,5 @@
 const user = require("../user")
+const database = require("../database/database.js")
 const express = require("express")
 const { checkAdmin } = require("../utility")
 const router = express.Router()
