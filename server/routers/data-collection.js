95e7bd11e474d85f39a88576dbf2eb60c3997f42
Igor96 <thedarthdot@gmail.com>
2023-02-02 20:05:45 -0500

    ncd

--- server/routers/data-collection.js
+++ server/routers/data-collection.js
@@ -5,7 +5,12 @@ const router = express.Router()
 
 router.get("/",  async function(req, res) { //only gets used if the url == data-collection
     const isAdmin = await checkAdmin(req)
+    let runningMatch = 0;
+    database.query(`select * from teamsixn_scouting_dev.current_game;`, (err, runningMatchResults) => {
+        runningMatch = runningMatchResults[0].cg_gm_number
+    })
     res.render("data-collection", {
+        runningMatch,
         user: user,
         isAdmin: isAdmin
     })
