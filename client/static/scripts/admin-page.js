61b260649cb63c94be4399c394bb3e3c7c84c101
Igor024 <thedarthdot@gmail.com>
2023-02-04 21:26:19 -0500

    gre

--- client/static/scripts/admin-page.js
+++ client/static/scripts/admin-page.js
@@ -1,4 +1,4 @@
-const { copy } = require("../../../server/routers/admin-page");
+import { paths } from "./utility.js"
 
 const observer = new MutationObserver(function (mutations_list) {
     mutations_list.forEach(function (mutation) {
@@ -13,9 +13,55 @@ const observer = new MutationObserver(function (mutations_list) {
 observer.observe(document.body, { subtree: false, childList: true });
 window.addEventListener("load", main)
 
+
+function assignUsers(data) {
+    return new Promise(resolve => {
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: paths.adminPage,
+            data: JSON.stringify(data),
+            success: function (response) {
+                if (response.response == true) {
+                    resolve([true])
+                }
+                else {
+                    resolve([false])
+                }
+            },
+
+            error: function (jqXHR, textStatus, errorThrown) {
+                resolve([false])
+            },
+        })
+    })
+}
+
 function main() {
-    var select  = document.getElementsByClassName("select-user")
-    select.addEventListener("changed", () => {
+    var selections  = Array.from(document.getElementsByClassName("select-user"))
+    const submitButton = document.getElementById("admin-submit")
+
+    submitButton.addEventListener("click", () => {
+        //animate the button clicAk effect
+        submitButton.style.backgroundColor = "#3b86cc"
+        submitButton.style.boxShadow = "0 2px #1c3750"
+        submitButton.style.transform = "translateY(4px)"
+
+        //get all the data
+        const data = new Array(selections.length)
+        for(let i = 0; i < selections.length; i++) {
+            data[i] = selections[i].value
+            console.log(selections[i].value)
+        }
         
+        //send post request
+        assignUsers(data)
+
+        //animate the button back
+        setTimeout(() => {
+            submitButton.style.backgroundColor = "#3492EA"
+            submitButton.style.boxShadow = "0 6px #3077b9"
+            submitButton.style.transform = ""
+        }, 100); //in milliseconds
     })
 }
