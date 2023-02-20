4abf563c36c354655e809618a3db3a45b3e990ee
BuildTools <unconfigured@null.spigotmc.org>
2023-02-20 18:37:09 -0500

    timestamp works

--- server/game.js
+++ server/game.js
@@ -1,9 +1,13 @@
 const YEAR = 2023;
 const COMP = "test"
 const GAME_TYPE = "Q"
+const gameStart = new Date(2023, 3, 1)
+const gameEnd = new Date(2023, 3, 4)
 
 module.exports = {
     YEAR: YEAR,
     COMP: COMP,
     GAME_TYPE: GAME_TYPE,
+    gameStart: gameStart,
+    gameEnd: gameEnd
 }
