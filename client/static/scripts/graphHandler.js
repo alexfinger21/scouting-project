1956c1a743e00ba3360390fe9a4334c63e6b0f6b
Igor96 <thedarthdot@gmail.com>
2023-02-23 20:55:08 -0500

    sad

--- client/static/scripts/graphHandler.js
+++ client/static/scripts/graphHandler.js
@@ -106,23 +106,50 @@ function createBarGraph(points, xAxisTitle, yAxisTitle) {
             legend: {
                 display: false
             },
-                //maintainAspectRatio: false,
+            //maintainAspectRatio: false,
             scales: {
                 xAxes: [{
                     position: "top",
                     scaleLabel: {
-                        display: true,
-                        labelString: xAxisTitle,
+                        display: false,
+                        //labelString: xAxisTitle,
                     },
                 }],
 
                 yAxes: [{
                     scaleLabel: {
-                        display: true,
-                        labelString: yAxisTitle,
+                        display: false,
+                        //labelString: yAxisTitle,
                     }
                 }],
             },
+
+            //display values next to bars
+            events: false,
+            tooltips: {
+                enabled: false
+            },
+            hover: {
+                animationDuration: 0
+            },
+            animation: {
+                duration: 1,
+                onComplete: function () {
+                    var chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
+                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'bottom';
+
+                    this.data.datasets.forEach(function (dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
+                        meta.data.forEach(function (bar, index) {
+                            var data = dataset.data[index];
+                            ctx.fillText(data, bar._model.x + 5, bar._model.y + 6);
+                        });
+                    });
+                }
+            }
         },
         plugins: {
             zoom: {
