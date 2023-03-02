322f6517a4e9976c7904a5647b1aade72ac6861c
BuildTools <unconfigured@null.spigotmc.org>
2023-03-02 17:35:29 -0500

    works

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,12 +1,12 @@
 const YEAR = 2023
-const EVENT_CODE = "flwp"
+const COMP = "flwp"
 const GAME_TYPE = "P"
 const gameStart = new Date("Febuary 28, 2023")
 const gameEnd = new Date("March 5, 2023")
 
 export{
     YEAR,
-    EVENT_CODE,
+    COMP,
     GAME_TYPE,
     gameStart,
     gameEnd
