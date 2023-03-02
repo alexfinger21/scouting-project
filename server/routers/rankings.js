499e331aeba78ab1441385ed603b1b8f4796b1ae
alexfinger21 <cdx9933@gmail.com>
2023-03-02 13:39:47 -0500

    nice

--- server/routers/rankings.js
+++ server/routers/rankings.js
@@ -12,7 +12,8 @@ function addZero(num) {
 
 router.get("/", async function (req, res) { //only gets used if the url == team-summary
     const isAdmin = await checkAdmin(req)
-    database.query(`SELECT * FROM teamsixn_scouting_dev.api_rankings`, (err, results) => {
+    database.query(`SELECT * FROM teamsixn_scouting_dev.api_rankings WHERE api_rankings.frc_season_master_sm_year = ${gameConstants.YEAR}
+    AND api_rankings.competition_master_cm_event_code = '${gameConstants.COMP} `, (err, results) => {
         results = JSON.parse(JSON.stringify(results))
 
         results.sort((a, b) => a.api_rank - b.api_rank)
