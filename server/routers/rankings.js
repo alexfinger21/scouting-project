15d4974fa404970346fb1b8abe2a6c354507e144
BuildTools <unconfigured@null.spigotmc.org>
2023-03-02 17:53:53 -0500

    d

--- server/routers/rankings.js
+++ server/routers/rankings.js
@@ -15,21 +15,22 @@ function addZero(num) {
 router.get("/", async function (req, res) { //only gets used if the url == team-summary
     const isAdmin = await checkAdmin(req)
     database.query(`SELECT * FROM teamsixn_scouting_dev.api_rankings WHERE api_rankings.frc_season_master_sm_year = ${gameConstants.YEAR}
-    AND api_rankings.competition_master_cm_event_code = '${gameConstants.COMP} `, (err, results) => {
+    AND api_rankings.competition_master_cm_event_code = '${game.COMP}'`, (err, results) => {
         results = JSON.parse(JSON.stringify(results))
 
         if (results) results.sort((a, b) => a.api_rank - b.api_rank)
         
 
         console.log(results)
-
+        console.log(gameConstants.gameStart.toLocaleString())
+        
         res.render("rankings", {
             user: user,
             isAdmin: isAdmin,
             year: results != null ? results[0].frc_season_master_sm_year : gameConstants.YEAR,
             eventCode: results != null ? results[0].competition_master_cm_event_code: gameConstants.COMP,
-            timeStamp: results != null ? results[0].api_rank_ts : gameConstants.gameStart.toLocaleString,
-            teamStats: results
+            timeStamp: results != null ? results[0].api_rank_ts : gameConstants.gameStart.toLocaleString(),
+            teamStats: results != null ? results : []
         })
     })
 
