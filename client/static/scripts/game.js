e03955638e3cfaccdc42c481cd60ba5b3711578b
alexfinger21 <cdx9933@gmail.com>
2023-03-02 13:39:47 -0500

    nice

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,12 +1,12 @@
 const YEAR = 2023
-const COMP = "flwp"
+const EVENT_CODE = "flwp"
 const GAME_TYPE = "P"
 const gameStart = new Date("Febuary 28, 2023")
 const gameEnd = new Date("March 5, 2023")
 
 export{
     YEAR,
-    COMP,
+    EVENT_CODE,
     GAME_TYPE,
     gameStart,
     gameEnd
