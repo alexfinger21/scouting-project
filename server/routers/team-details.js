530c652454936fd1cbcff247292d8fbb4ec07ecd
Igor024 <thedarthdot@gmail.com>
2023-03-02 21:34:04 -0500

    team details fix

--- server/routers/team-details.js
+++ server/routers/team-details.js
@@ -6,28 +6,36 @@ const router = express.Router()
 
 router.get("/", function (req, res) { //only gets used if the url == team-details
     console.log("recieved")
-    const team = req.query.team ? req.query.team : 695
-
     database.query(`SELECT 
-        * 
+        DISTINCT team_master_tm_number 
         FROM 
             teamsixn_scouting_dev.v_match_team_score vmts
         WHERE
             vmts.frc_season_master_sm_year = ${gameConstants.YEAR} AND
             vmts.competition_master_cm_event_code = '${gameConstants.COMP}' AND 
-            vmts.game_matchup_gm_game_type = '${gameConstants.GAME_TYPE}' AND
-            vmts.team_master_tm_number = ${team};`,
-        (err, results) => {
-            results = JSON.parse(JSON.stringify(results))
+            vmts.game_matchup_gm_game_type = '${gameConstants.GAME_TYPE}';`,
+        (err, team_results) => {
+            let team = req.query.team
+            if(team == null) {
+                if(team_results != null && team_results[0] != null) {
+                    team = team_results[0].team_master_tm_number
+                }
+                else {
+                    team = 695
+                }
+            }
             database.query(`SELECT 
-                DISTINCT team_master_tm_number 
+                * 
                 FROM 
                     teamsixn_scouting_dev.v_match_team_score vmts
                 WHERE
                     vmts.frc_season_master_sm_year = ${gameConstants.YEAR} AND
                     vmts.competition_master_cm_event_code = '${gameConstants.COMP}' AND 
-                    vmts.game_matchup_gm_game_type = '${gameConstants.GAME_TYPE}';`,
-                (err, team_results) => {
+                    vmts.game_matchup_gm_game_type = '${gameConstants.GAME_TYPE}' AND
+                    vmts.team_master_tm_number = ${team};`,
+                (err, results) => {
+                    results = JSON.parse(JSON.stringify(results))
+
                     console.log("RESULTS: ")
                     console.log(results)
                     res.render("team-details", {
