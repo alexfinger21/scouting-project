6a54d33166097d54bf465ec0927588ed618376d1
Igor024 <thedarthdot@gmail.com>
2023-03-02 19:09:57 -0500

    alliance input complete

--- server/routers/alliance-input.js
+++ server/routers/alliance-input.js
@@ -14,9 +14,48 @@ router.get("/", function (req, res) { //only gets used if the url == team-detail
     ORDER BY
         team_master_tm_number;`,
     (err, team_results) => {
+        team_results = JSON.parse(JSON.stringify(team_results))
         if (!req.query.getTeams) {
-            res.render("alliance-input", {
-                teams: team_results.map(e => e.team_master_tm_number).sort((a, b) => a - b)
+            database.query(`select * from teamsixn_scouting_dev.v_alliance_selection_display`, (err, data) => {
+                data = JSON.parse(JSON.stringify(data))
+                console.log("DATA:")
+                console.log(data)
+
+                let selectedTeams = []
+                for(let i = 0; i < data.length; i++) {
+                    if(data[i]) {
+                        if(data[i].alliance_captain) {
+                            selectedTeams.push(data[i].alliance_captain)
+                        }
+                        if(data[i].alliance_first) {
+                            selectedTeams.push(data[i].alliance_first)
+                        }
+                        if(data[i].alliance_second) {
+                            selectedTeams.push(data[i].alliance_second)
+                        }
+                    }
+                }
+                console.log("SELECTED TEAMS")
+                console.log(selectedTeams)
+                let availableTeams = []
+                for(team of team_results) {
+                    let found = false
+                    for(selected of selectedTeams) {
+                        if(team.team_master_tm_number == selected) {
+                            found = true
+                        }
+                    }
+                    if(!found) {
+                        availableTeams.push(team.team_master_tm_number)
+                    }
+                }
+                console.log("AVAILABLE TEAMS")
+                console.log(availableTeams)
+
+                res.render("alliance-input", {
+                    teams: availableTeams,
+                    data: data
+                })
             })
         } else {
             return res.status(200).send({teams: team_results.map(e => e.team_master_tm_number).sort((a, b) => a - b)})
@@ -28,10 +67,15 @@ router.post("/", function (req, res) {
     const body = req.body
     console.log("BODY: ")
     console.log(body)
-    database.query(database.deleteAllianceSelection(body.allianceNum, body.pos, body.team), (err, result) => {console.log(result)})
-    if (body.action == "INSERT") {
-        database.query(database.insertAllianceSelection(body.allianceNum, body.pos, body.team), (err, result) => {console.log(result)})
-    }
+    database.query(database.deleteAllianceSelection(body.allianceNum, body.pos, body.team), (err, result) => {
+        console.log(err)
+        console.log("REMOVED")
+        if (body.action == "INSERT") {
+            database.query(database.insertAllianceSelection(body.allianceNum, body.pos, body.team), (err, result) => {console.log(err)})
+            console.log("INSERTED")
+        }
+    })
+    res.send("req recieved")
 })
 
 module.exports = router
