1d17f594e2ca0c2386a7d2e36e36a22208fc3ba9
Igor024 <thedarthdot@gmail.com>
2023-03-03 20:47:03 -0500

    rounded values in team summary and match strategy, also available teams works in alliance selector

--- client/static/scripts/alliance-selector.js
+++ client/static/scripts/alliance-selector.js
@@ -14,37 +14,44 @@ const observer = new MutationObserver(function (mutations_list) {
 observer.observe(document.body, { subtree: false, childList: true });
 window.addEventListener("load", main)
 
+function getAvailableTeams(sortValue) {
+    //delete everything inside
+    const table = document.getElementById("available-teams-table")
+    const tbody = table.getElementsByTagName("tbody")[0]
+
+    tbody.replaceChildren() //replaces all the children with an array of new children
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: paths.allianceSelector,
+        data: JSON.stringify({ sortBy: sortValue }),
+        success: function (response) {
+            console.log(response)
+
+
+
+            console.log(tbody)
+
+            for (const team of response) {
+                const html = "<tr><td>" + team.team + "</td><td>" + team.apiRank + "</td><td>" + Math.round(team.gameScore) + "</td><td><td></td><td>" + Math.round(team.chargeStation) + "</td></tr> "
+                tbody.insertAdjacentElement("beforeend", $(html)[0])
+            }
+        },
+
+        error: function (jqXHR, textStatus, errorThrown) {
+
+        },
+    })
+}
+
 function main() {
     //when a team buton is clicked, make it empty
     const sortBy = document.getElementById("sorting-options")
 
+    getAvailableTeams(sortBy.value)
+
     sortBy.addEventListener("change", (e) => {
-        const sortValue = sortBy.value
-
-        console.log(sortValue)
-
-        $.ajax({
-            type: "POST",
-            contentType: "application/json",
-            url: paths.allianceSelector,
-            data: JSON.stringify({sortBy: sortValue}),
-            success: function (response) {
-                console.log(response)
-
-                const table = document.getElementById("available-teams-table")
-                const tbody = table.getElementsByTagName("tbody")[0]
-
-                console.log(tbody)
-
-                for(const team of response) {
-                    const html = "<tr> <td> " + team.team + " </td> <td> " + team.gameScore + "</td> " + " </tr> " 
-                    tbody.insertAdjacentElement("afterend", $(html)[0])
-                }
-            },
-
-            error: function (jqXHR, textStatus, errorThrown) {
-
-            },
-        })
+        getAvailableTeams(sortBy.value)
     })
 }
