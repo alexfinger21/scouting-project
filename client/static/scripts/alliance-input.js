0a658cd73f7f622798e735cbdb70bab73238a943
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 22:26:22 -0500

    d

--- client/static/scripts/alliance-input.js
+++ client/static/scripts/alliance-input.js
@@ -2,6 +2,7 @@ import { socket, paths, requestData, requestPage, currentPage} from "./utility.j
 
 socket.on("allianceSelection", (match_num) => {
     console.log("nice")
+    console.log(currentPage)
     if (currentPage == paths.allianceInput.substring(1)) {
         requestPage(paths.allianceInput)
     }
