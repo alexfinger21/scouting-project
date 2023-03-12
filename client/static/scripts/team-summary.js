27276457f42ab8f4c10953a7aa38842dd3da0103
Igor024 <thedarthdot@gmail.com>
2023-03-12 18:58:46 -0400

    sad

--- client/static/scripts/team-summary.js
+++ client/static/scripts/team-summary.js
@@ -5,7 +5,7 @@ const POINT_COLOR = "rgb(147, 157, 168)"
 const OUR_TEAM_COLOR = "rgb(242, 142, 43)"
 const HIGHTLIGHT_COLOR = "rgb(158, 225, 87)"
 
-let debounce = false
+let debounce = false 
 
 //When teamsummary is loaded, call the main function 
 const observer = new MutationObserver(function (mutations_list) {
