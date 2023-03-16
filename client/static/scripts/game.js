92c83a0f7762fffca6c2c3a37c7b70c7fb8444dd
alexfinger21 <cdx9933@gmail.com>
2023-03-16 09:03:06 -0400

    sa

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,6 +1,6 @@
 const YEAR = 2023
-const COMP = "ohmv"
-const GAME_TYPE = "P"
+const COMP = "test"
+const GAME_TYPE = "Q"
 const gameStart = new Date("Febuary 28, 2023")
 const gameEnd = new Date("March 5, 2023")
 
