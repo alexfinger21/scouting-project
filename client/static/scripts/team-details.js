a5c190f3cf4c44147ca8c79e16fc76989ea24ba2
Igor96 <thedarthdot@gmail.com>
2023-04-08 12:38:42 -0400

    sada

--- client/static/scripts/team-details.js
+++ client/static/scripts/team-details.js
@@ -16,6 +16,7 @@ window.addEventListener("load", main)
 
 function main() {
     const teamSelector = document.querySelector("#team-display-selector")
+    const pitScoutingButton = document.getElementById("pit-scouting-button")
     const pictureContainer = document.getElementById("team-pictures-container")
     const arrowLeft = document.getElementById("arrow-left")
     const arrowRight = document.getElementById("arrow-right")
@@ -25,6 +26,25 @@ function main() {
         consoleLog("hi")
         const data = await requestPage(paths.teamDetails + "?team=" + teamSelector.value, {}, paths.teamDetails)
 
+<<<<<<< Updated upstream
         consoleLog(data)
     }) 
+=======
+        console.log(data)
+    })
+
+    pitScoutingButton.addEventListener("click", (e) => {
+        const pitScoutingContainer = document.getElementById("pit-scouting-container")
+        console.log(pitScoutingContainer.style.display)
+        if(pitScoutingContainer.style.display == "none") {
+            console.log("wow")
+            pitScoutingContainer.style.display == "block"
+        }
+        else {
+            console.log("not cool")
+            pitScoutingContainer.style.display == "none"
+        }
+         
+    })
+>>>>>>> Stashed changes
 }
