41e02febdcd75e60aaf6b4eb43f505681144a370
Igor96 <thedarthdot@gmail.com>
2023-04-20 09:23:16 -0400

    d

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,8 +1,8 @@
 const YEAR = 2023
 const COMP = "dal"
 const GAME_TYPE = "Q"
-const gameStart = new Date("March 29, 2023")
-const gameEnd = new Date("April 2, 2023")
+const gameStart = new Date("April 19, 2023")
+const gameEnd = new Date("April 25, 2023")
 
 export {
     YEAR,
