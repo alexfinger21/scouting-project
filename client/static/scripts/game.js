a1beb39d77ba8bf0e9fefac860ce2b5532723d3d
alexfinger21 <cdx9933@gmail.com>
2023-09-21 19:30:43 -0400

    new game types

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,8 +1,8 @@
 const YEAR = 2023
-const COMP = "dal"
+const COMP = "test"
 const GAME_TYPE = "Q"
-const gameStart = new Date("April 19, 2023")
-const gameEnd = new Date("April 25, 2023")
+const gameStart = new Date("September 20, 2023")
+const gameEnd = new Date("September 25, 2023")
 
 export {
     YEAR,
