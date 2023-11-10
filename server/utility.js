f6107a6f4c6564b478b4571337a0d5117538500a
Alex Rabochii <cdx9933@gmail.com>
2023-11-10 17:24:27 -0500

    alliance selection algorithm work in progress

--- server/utility.js
+++ server/utility.js
@@ -1,6 +1,17 @@
 const database = require("./database/database.js")
 const log = true
 
+function rank(arr) {
+    const arraySorted = arr.slice().sort((a, b) => b - a)
+    return arr.map(v => {
+        return arraySorted.indexOf(v)
+    })
+}
+
+function arrAvg(...args) {
+    return args.reduce((total, val) => total+val)/args.length
+}
+
 function consoleLog(arg) {
     if (toString(arg).indexOf("frc_season_master_sm_year") != -1) {
         console.log("\nFOUND THE MASSIVE PRINT STATEMENT: ")
@@ -29,5 +40,20 @@ function checkAdmin(req) {
     })
 }
 
+function suggestTeam(currentAlliance, otherAlliances) {
+    const teamStats = {
+        gameScore: arrAvg(...currentAlliance.map(t => t.gameScore)),
+        links: arrAvg(...currentAlliance.map(t => t.links)),
+        autonDock: arrAvg(...currentAlliance.map(t => t.autonDock)),
+        endGameDock: arrAvg(...currentAlliance.map(t => t.endGameDock)),
+    }
+    /*
+    const GSRank = rank(data.map(e => e.avg_gm_score))
+    const linkRank = rank(data.map(e => e.avg_nbr_links))
+    const autonCSRank = rank(data.map(e => e.avg_auton_chg_station_score))
+    const endGameCSRank = rank(data.map(e => e.avg_endgame_chg_station_score))
+    */
+}
 
-module.exports = { checkAdmin: checkAdmin, consoleLog: consoleLog }
+
+module.exports = { checkAdmin: checkAdmin, consoleLog: consoleLog, suggestTeam: suggestTeam}
