758004d224976ed7f469877f2e959fec341aabd3
alexfinger21 <cdx9933@gmail.com>
2023-11-12 21:48:55 -0500

    april 15

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -4,10 +4,10 @@ const GAME_TYPE = "Q"
 const gameStart = new Date("April 15, 2023")
 const gameEnd = new Date("April 30, 2023")
 
-module.exports = {
-    YEAR: YEAR,
-    COMP: COMP,
-    GAME_TYPE: GAME_TYPE,
-    gameStart: gameStart,
-    gameEnd: gameEnd
+export {
+    YEAR,
+    COMP,
+    GAME_TYPE,
+    gameStart,
+    gameEnd
 }
