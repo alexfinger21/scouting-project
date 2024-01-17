4d80789d2bd470af2bf732442712d5b85991f0ea
igor023 <thedarthdot@gmail.com>
2024-01-17 01:44:39 -1000

    Robot class

---
+++ client/static/scripts/data_collection/Robot.js
@@ -0,0 +1,18 @@
+class Robot extends DrawableObject {
+    /* 
+    Ctx: canvas.getcontext2dallianceColor: "red" or "blue", x: pixels from left
+    y: pixels from top
+    */
+   
+    constructor(x, y, ctx) {
+        let img = new Image()
+        if(allianceColor == blue) {
+            img.src = "../../../images/data-collection/blue-robot.png"
+        }
+        else {
+            img.src = "../../../images/data-collection/red-robot.png"
+        }
+
+        super(ctx, this.img, x, y, 200, 200);
+    }
+}
