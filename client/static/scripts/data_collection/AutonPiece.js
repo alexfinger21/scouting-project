4b455d4e18360cdd127c12dc4803423640150ccd
alexfinger21 <cdx9933@@gmail.com>
2024-01-16 20:53:01 -0500

    auton piece update

--- client/static/scripts/data_collection/AutonPiece.js
+++ client/static/scripts/data_collection/AutonPiece.js
@@ -1,1 +1,13 @@
+export default class extends DrawableObject {
+    constructor(x, y) {
+        let img = new Image()
+        if(allianceColor == blue) {
+            img.src = "../../../images/data-collection/orange-note.png"
+        }
+        else {
+            img.src = "../../../images/data-collection/orange-note.png"
+        }
 
+        super(ctx, this.img, x, y, 200, 200);
+    }
+}
