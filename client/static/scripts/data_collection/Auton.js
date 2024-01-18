707e121c1b59d21d073a28c5652266f9bff98679
alexfinger21 <cdx9933@gmail.com>
2024-01-17 22:27:00 -0500

    added image loading waits

--- client/static/scripts/data_collection/Auton.js
+++ client/static/scripts/data_collection/Auton.js
@@ -1,26 +1,21 @@
-import { consoleLog } from "../utility"
-import AutonMap from "./AutonMap"
-import PiecesMap from "./PiecesMap"
-import Robot from "./Robot"
+import AutonMap from "./AutonMap.js"
+import Robot from "./Robot.js"
 
 export default class {
     /*ctx: canvas.getContext('2d')
-    allianceColor: "red", "blue" */
+    allianceColor: "R", "B" */
     constructor({ctx, allianceColor}) {
         this.ctx = ctx
-        this.map = new AutonMap(ctx, allianceColor)
-        this.robot = new Robot(ctx, allianceColor)
-        this.pieces = new PiecesMap(ctx, allianceColor)
+        this.map = new AutonMap({ctx, allianceColor, x: 50, y: 100})
+        this.robot = new Robot({ctx, allianceColor})
     }
 
     draw() {
-        ctx.save()
+        //this.ctx.save()
 
-        for (const piece in this.pieces) {
-            //do stuff
-            consoleLog(piece)
-        }
+        this.map.draw()
+        this.robot.draw()
 
-        ctx.restore()
+        //this.ctx.restore()
     }
 }
--- client/static/scripts/data_collection/gamePiece.js
+++ client/static/scripts/data_collection/gamePiece.js
@@ -1,8 +1,8 @@
-import DrawableObject from "./DrawableObject";
+import DrawableObject from "./DrawableObject.js"
 
 export default class extends DrawableObject {
     constructor({x, y, ctx}) {
-        let img = new Image()
+        const img = new Image()
         img.src = "../../../images/data-collection/orange-note.png"
 
         super({ctx, img, x, y, sX: 200, sY: 200});
