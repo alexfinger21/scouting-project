9482be629310fa71a7a6e97975728e380bb9d085
igor024 <thedarthdot@gmail.com>
2024-01-21 11:21:32 -0500

    Piecesmap respects alliance color

--- client/static/scripts/data_collection/PiecesMap.js
+++ client/static/scripts/data_collection/PiecesMap.js
@@ -7,11 +7,13 @@ export default class PiecesMap {
         console.log(canvasSize)
         this.pieces = new Array(8)
         //Add Pickup Wing Notes ge_key 202-204
+        const wingNoteX = allianceColor == "B" ? Math.floor(canvasSize.x*0.25) : Math.floor(canvasSize.x*0.65)
+        const centerNoteX = allianceColor == "B" ? Math.floor(canvasSize.x*0.8) : Math.floor(canvasSize.x*0.05)
         for(let i = 0; i < 3; i++) {
-            this.pieces[i] = new GamePiece({x: Math.floor(canvasSize.x*0.25), y: canvasSize.y*0.1 + Math.floor(canvasSize.y*0.18*i), ctx, img, ge_key: 202+i, canvasSize})
+            this.pieces[i] = new GamePiece({x: wingNoteX, y: canvasSize.y*0.1 + Math.floor(canvasSize.y*0.18*i), ctx, img, ge_key: 202+i, canvasSize})
         }
         for(let i = 0; i < 5; i++) {
-            this.pieces[3+i] = new GamePiece({x: Math.floor(canvasSize.x*0.8), y: canvasSize.y*0.1+Math.floor(canvasSize.y*0.18*i), ctx, img, ge_key: 205+i, canvasSize})
+            this.pieces[3+i] = new GamePiece({x: centerNoteX, y: canvasSize.y*0.1+Math.floor(canvasSize.y*0.18*i), ctx, img, ge_key: 205+i, canvasSize})
         }
         console.log(this.pieces)
     }
