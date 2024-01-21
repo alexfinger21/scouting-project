8cd0fdbf20281785d3c256f681a680bfc0135f7d
igor024 <thedarthdot@gmail.com>
2024-01-21 14:05:22 -0500

    Fix NaN note color lerp bug co-authored by alexfinger21

--- client/static/scripts/data_collection/PiecesMap.js
+++ client/static/scripts/data_collection/PiecesMap.js
@@ -7,10 +7,10 @@ export default class PiecesMap {
         console.log(canvasSize)
         this.pieces = new Array(8)
         //Add Pickup Wing Notes ge_key 202-204
-        const wingNoteX = allianceColor == "B" ? Math.floor(canvasSize.x*0.25) : Math.floor(canvasSize.x*0.65)
+        const wingNoteX = allianceColor == "B" ? Math.floor(canvasSize.x*0.3) : Math.floor(canvasSize.x*0.65)
         const centerNoteX = allianceColor == "B" ? Math.floor(canvasSize.x*0.8) : Math.floor(canvasSize.x*0.05)
         for(let i = 0; i < 3; i++) {
-            this.pieces[i] = new GamePiece({x: wingNoteX, y: canvasSize.y*0.1 + Math.floor(canvasSize.y*0.18*i), ctx, img, ge_key: 202+i, canvasSize})
+            this.pieces[i] = new GamePiece({x: wingNoteX, y: canvasSize.y*0.17 + Math.floor(canvasSize.y*0.18*i), ctx, img, ge_key: 202+i, canvasSize})
         }
         for(let i = 0; i < 5; i++) {
             this.pieces[3+i] = new GamePiece({x: centerNoteX, y: canvasSize.y*0.1+Math.floor(canvasSize.y*0.18*i), ctx, img, ge_key: 205+i, canvasSize})
