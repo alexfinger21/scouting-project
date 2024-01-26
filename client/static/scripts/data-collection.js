245cd62d76284e779c0bed84a6b4428ebaa7cfa8
igor024 <thedarthdot@gmail.com>
2024-01-25 20:15:18 -0500

    Rename auton container div

--- client/static/scripts/data-collection.js
+++ client/static/scripts/data-collection.js
@@ -2,6 +2,8 @@ import { clamp, currentPage, paths, requestPage, socket, getMatch, consoleLog } 
 import { moveToPage, setSelectedObject } from "./bottomBar.js"
 import { YEAR, COMP, GAME_TYPE } from "./game.js"
 import Auton from "./data_collection/Auton.js"
+//import Endgame from "./data_collection/Endgame.js"
+
 
 const timer = ms => new Promise((res, rej) => setTimeout(res, ms))
 let AutonObject
@@ -187,8 +189,8 @@ function loadData() {
         const localData = JSON.parse(localStorage.getItem("data"))
         const allianceColor = form.getAttribute("alliance")
         const alliancePosition = form.getAttribute("alliance-position")
-        const autonCanvasContainer = document.getElementById("auton-container")
         const autonCanvas = document.getElementById("auton-canvas")
+        const autonCanvasContainer = autonCanvas.parentElement
         const autonCanvasCTX = autonCanvas.getContext("2d") 
 
         if (!localData) {
