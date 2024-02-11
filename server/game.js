8cdb56d4d982beafe7ea18ee21013b9201e1a461
Mannakitam <mayer.bergman@gmail.com>
2024-02-11 12:26:50 -0500

    time

--- server/game.js
+++ server/game.js
@@ -3,11 +3,21 @@ const COMP = "test"
 const GAME_TYPE = "Q"
 const gameStart = new Date("April 15, 2023")
 const gameEnd = new Date("April 30, 2023")
-
+const sch_constants =
+    [[1, 10, '2023-03-17', '09:00:00', 600000],
+    [11, 22, '2023-03-17', '10:40:00', 420000], 
+    [23, 60, '2023-03-17', '13:00:00', 420000], 
+    [61, 87, '2023-03-18', '09:00:00', 420000]]
 module.exports = {
     YEAR: YEAR,
     COMP: COMP,
     GAME_TYPE: GAME_TYPE,
     gameStart: gameStart,
+<<<<<<< Updated upstream
     gameEnd: gameEnd
 }
+=======
+    gameEnd: gameEnd,
+    sch_constants: sch_constants
+}
+>>>>>>> Stashed changes
