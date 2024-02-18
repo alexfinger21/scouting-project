5d173e1f86483c7a4d09d7c0f37921b4a5576005
Igor024 <thedarthdot@gmail.com>
2024-02-18 02:32:16 -0500

    radar chart starting to work

--- client/static/scripts/utility.js
+++ client/static/scripts/utility.js
@@ -27,33 +27,6 @@ function consoleLog(...args) {
         }
     }
 }
-
-
-function hslToRgb(h, s, l) {
-    let r, g, b;
-
-    if (s === 0) {
-        r = g = b = l; // achromatic
-    } else {
-        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        const p = 2 * l - q;
-        r = hueToRgb(p, q, h + 1 / 3);
-        g = hueToRgb(p, q, h);
-        b = hueToRgb(p, q, h - 1 / 3);
-    }
-
-    return [round(r * 255), round(g * 255), round(b * 255)];
-}
-
-function hueToRgb(p, q, t) {
-    if (t < 0) t += 1;
-    if (t > 1) t -= 1;
-    if (t < 1 / 6) return p + (q - p) * 6 * t;
-    if (t < 1 / 2) return q;
-    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-    return p;
-}
-
 function hexToRgb(hex) {
     return [parseInt(hex.slice(1, 3), 16), parseInt(hex.slice(3, 5), 16), parseInt(hex.slice(5, 7), 16)]
 }
@@ -184,4 +157,4 @@ async function requestPage(url, data, pageVal) {
 }
 
 
-export {hslToRgb, hexToRgb, consoleLog, canvasFPS, lerp, socket, currentPage, clamp, selectRandom, getColor, requestPage, paths, arrHasDuplicates, getMatch, requestData, highlightColors}
+export {consoleLog, canvasFPS, lerp, socket, currentPage, clamp, selectRandom, getColor, requestPage, paths, arrHasDuplicates, getMatch, requestData, highlightColors}
