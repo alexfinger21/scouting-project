7f02578a12cddd6f65b5bc0998f2da892dcf5e93
igor023 <thedarthdot@gmail.com>
2024-02-29 03:44:26 -1000

    gamejs

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,8 +1,8 @@
 const YEAR = 2024
-const COMP = "test"
-const GAME_TYPE = "Q"
-const gameStart = new Date("April 15, 2023")
-const gameEnd = new Date("April 30, 2023")
+const COMP = "paca"
+const GAME_TYPE = "P"
+const gameStart = new Date("Febuary 29, 2024")
+const gameEnd = new Date("March 10, 2024")
 
 export {
     YEAR,
