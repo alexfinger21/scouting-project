07985232e0ffc42c47c32d9619ffe4bf683aa00c
igor023 <thedarthdot@gmail.com>
2024-02-29 03:44:26 -1000

    gamejs

--- server/game.js
+++ server/game.js
@@ -1,13 +1,13 @@
 const YEAR = 2024
-const COMP = "test"
-const GAME_TYPE = "Q"
-const gameStart = new Date("April 15, 2023")
-const gameEnd = new Date("April 30, 2023")
+const COMP = "paca"
+const GAME_TYPE = "P"
+const gameStart = new Date("Febuary 29, 2024")
+const gameEnd = new Date("March 10, 2024")
 const sch_constants =
-    [[1, 10, '2023-03-17', '09:00:00', 600000],
-    [11, 22, '2023-03-17', '10:40:00', 420000], 
-    [23, 60, '2023-03-17', '13:00:00', 420000], 
-    [61, 87, '2023-03-18', '09:00:00', 420000]]
+    [[1, 10, '2024-02-29', '09:00:00', 600000],
+    [11, 22, '2024-02-29', '10:40:00', 420000], 
+    [23, 60, '2024-02-29', '13:00:00', 420000], 
+    [61, 87, '2024-02-30', '09:00:00', 420000]]
 module.exports = {
     YEAR: YEAR,
     COMP: COMP,
