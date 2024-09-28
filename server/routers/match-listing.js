ef6410a7f63f705b68203542771ca97974b4e676
alexfinger21 <cdx9933@gmail.com>
2024-09-27 22:36:13 -0400

    details fixed

--- server/routers/match-listing.js
+++ server/routers/match-listing.js
@@ -9,7 +9,7 @@ const { data } = require("jquery")
 const { consoleLog } = require("../utility")
 const SQL = require('sql-template-strings')
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 process.env.lastPlayedMatch = 1
 
