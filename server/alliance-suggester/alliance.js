320629ab7f313aebddafdbefdb9ce142965eab9c
alexfinger21 <cdx9933@gmail.com>
2024-12-26 13:54:00 -0500

    weight comparison

--- server/alliance-suggester/alliance.js
+++ server/alliance-suggester/alliance.js
@@ -1,12 +1,34 @@
 const Team = require("./team.js")
 
+// teams: {1: Team, 2: Team, 3: Team}
+
 module.exports = class Alliance {
     constructor(teams) {
         this.teams = teams
     }
 
     static getWeights(comp, src) {
+        const [compAvg, srcAvg] = [comp.getAverage(), src.getAverage()]
+        const weights = {}
 
+        let maxW = 0
+
+        for (const x of Object.keys(compAvg)) {
+            weights[x] = (compAvg[x]/srcAvg[x])
+
+            if (weights[x] == Infinity) {
+                weights[x] = 1
+            }
+            maxW = Math.max(maxW, weights[x])
+        }
+
+        // normalize
+
+        for (const x of Object.keys(weights)) {
+            weights[x] = weights[x]/maxW
+        }
+
+        return weights
     }
 
     getAverage() {
