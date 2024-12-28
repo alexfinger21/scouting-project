edee8910a1e2d7f6f68c2ab984a7770717071ddf
alexfinger21 <cdx9933@gmail.com>
2024-12-28 00:35:00 -0500

    weight averaging

--- server/alliance-suggester/team.js
+++ server/alliance-suggester/team.js
@@ -4,4 +4,33 @@ module.exports = class Team {
         this.props = props
         this.suggestions = {}
     }
+
+    static getAverage(...teams) {
+        const res = {}
+        const tlen = teams.filter(t => t).length
+
+        for (const t of teams) {
+            if (t && t.props) {
+                for (const p of Object.keys(t.props)) {
+                    if (!res[p]) {
+                        res[p] = t.props[p]/tlen
+                    } else {
+                        res[p] += t.props[p]/tlen
+                    }
+                }
+            }
+        }
+
+        return res
+    }
+
+    getRank(comp, weights) {
+        let res = 0 
+
+        for (const i of Object.keys(comp)) {
+            res += this.props[i]/comp[i] * weights[i]
+        }
+
+        return res
+    }    
 }
