3f51759df7f7c62388cedcdb50130e8b6b5cd515
alexfinger21 <cdx9933@gmail.com>
2025-03-09 13:00:53 -0400

    constants

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,8 +1,14 @@
 const YEAR = 2024
-const COMP = "ohcl"
+const COMP = "caph"
 const GAME_TYPE = "Q"
 const gameStart = new Date("Octoer 20, 2024")
 const gameEnd = new Date("October 30, 2024")
+const sch_constants =
+    [[1, 9, '2024-03-22', '09:00:00', 600000],
+    [10, 21, '2024-03-22', '10:28:00', 480000], 
+    [22, 57, '2024-03-22', '13:00:00', 480000], 
+    [58, 80, '2024-03-23', '09:00:00', 480000]]
+
 
 export {
     YEAR,
