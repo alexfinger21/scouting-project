439d8e9546e19a61948af3d53c094c58408c6dad
alexfinger21 <cdx9933@gmail.com>
2025-03-27 16:21:35 -0400

    quals test

--- client/static/scripts/game.js
+++ client/static/scripts/game.js
@@ -1,8 +1,8 @@
 const YEAR = 2025
-const COMP = "nyro"
+const COMP = "test"
 const GAME_TYPE = "Q"
 const gameStart = new Date("March 12, 2025")
-const gameEnd = new Date("March 17, 2025")
+const gameEnd = new Date("April 17, 2025")
 const sch_constants =
     [[1, 9, '2025-03-', '09:00:00', 600000],
     [10, 21, '2024-03-22', '10:28:00', 480000], 
