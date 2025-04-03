2912539dc0a7ac91fe90d6325f5f1f061364721b
alexfinger21 <cdx9933@gmail.com>
2025-04-03 12:41:09 -0400

    hotfix

--- client/static/scripts/data_collection/Path.js
+++ client/static/scripts/data_collection/Path.js
@@ -15,19 +15,21 @@ export default class Path {
         for (let i = 1; i<this.path.length; ++i) {
             let p1;
 
-            if (Math.abs(this.path[i-1].y - this.canvasSize.y/2) > Math.abs(this.path[i].y - this.canvasSize.y/2)) {
-                p1 = {x: this.path[i].x, y: this.path[i-1].y}
-            } else {
-                p1 = {x: this.path[i-1].x, y: this.path[i].y}
-            }
+            if (this?.path?.[i]?.x && this?.path?.[i]?.y && this?.path?.[i-1]?.x && this?.path?.[i-1]?.y) {
+                if (Math.abs(this.path[i-1].y - this.canvasSize.y/2) > Math.abs(this.path[i].y - this.canvasSize.y/2)) {
+                    p1 = {x: this.path[i].x, y: this.path[i-1].y}
+                } else {
+                    p1 = {x: this.path[i-1].x, y: this.path[i].y}
+                }
 
-            lines.push(new QuadraticLine({
-                ctx: this.ctx, 
-                canvasSize: this.canvasSize,
-                p0: this.path[i-1],
-                p1,
-                p2: this.path[i]
-            }))
+                lines.push(new QuadraticLine({
+                    ctx: this.ctx, 
+                    canvasSize: this.canvasSize,
+                    p0: this.path[i-1],
+                    p1,
+                    p2: this.path[i]
+                }))
+            }
         }
 
         lines.forEach(l => this.renderQueue.insert(l))
