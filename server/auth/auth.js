da8983aaf174236f24199f3b8e2315a3a758c290
alexfinger21 <cdx9933@gmail.com>
2025-09-05 18:25:56 -0400

    rawr

---
+++ server/auth/auth.js
@@ -0,0 +1,13 @@
+const {SDK, SdkConfig} = require("casdoor-nodejs-sdk")
+
+const sdkConfig  = {
+    serverUrl: "https://sso.team695.com",
+    clientId: "8f4953fcb962d4f7c823",
+    appName: "695_scoutify_webapp",
+    organizationName: "Team695",
+    redirectPath: "/callback",
+    signinPath: "/api/signin",
+}
+const sdk = new SDK(sdkConfig)
+
+module.exports = { sdk }
