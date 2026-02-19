451f415c827b94d62cce1c497080dc8a46fa3369
alexfinger21 <cdx9933@gmail.com>
2026-02-18 21:43:27 -0500

    logout

--- server/routers/api.js
+++ server/routers/api.js
@@ -40,9 +40,6 @@ router.get("/getUserInfo", async function (req, res) {
     } catch(err) {
         console.log("err while trying to access user: ", err)
     }
-    
-    console.log(user)
-
 
     return res.send({
         name: user.name,
