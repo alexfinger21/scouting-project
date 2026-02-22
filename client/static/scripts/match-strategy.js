def9707572efc6c4de84067b53852d06cb85e5ed
Mannakitam <mayer.bergman@gmail.com>
2026-02-22 17:52:37 -0500

    started changing over match strategy

--- client/static/scripts/match-strategy.js
+++ client/static/scripts/match-strategy.js
@@ -64,42 +64,42 @@ async function main() {
     const red = data.slice(0, 3)
     const blue = data.slice(3)
 
-    const config = await graphHandler.createSpiderChart(
-        [
-            {
-                teamNumber: 0,
-                teamName: "Red Alliance",
-                color: "rgb(255,0,0)",
-                hidden: false,
-                autonScore: sumParam(red, "auton_total_score_avg"),
-                gamesPlayed: sumParam(red, "nbr_games"),
-                gameScore: sumParam(red, "total_game_score_avg"),
-                teleopScore: sumParam(red, "teleop_total_score_avg"),
-                endgameScore: sumParams(red, ["endgame_park_avg", 2], ["endgame_shallow_climb_avg", 6], ["endgame_deep_climb_avg", 12]),
-                rank: sumParam(red, "api_rank"),
-                opr: sumParam(red, "api_opr"),
-                dpr: sumParam(red, "api_dpr")
-            },
-            {
-                teamNumber: 1,
-                teamName: "Blue Alliance",
-                color: "rgb(0,0,255)",
-                hidden: false,
-                autonScore: sumParam(blue, "auton_total_score_avg"),
-                gamesPlayed: sumParam(blue, "nbr_games"),
-                gameScore: sumParam(blue, "total_game_score_avg"),
-                teleopScore: sumParam(blue, "teleop_total_score_avg"),
-                endgameScore: sumParams(blue, ["endgame_park_avg", 2], ["endgame_shallow_climb_avg", 6], ["endgame_deep_climb_avg", 12]),
-                rank: sumParam(blue, "api_rank"),
-                opr: sumParam(blue, "api_opr"),
-                dpr: sumParam(blue, "api_dpr")
-            }
-        ],
-        false
-    )
-    consoleLog("CONF:")
-    consoleLog(config)
-    chart = new Chart(ctx,
-        config
-    )
+    // const config = await graphHandler.createSpiderChart(
+    //     [
+    //         {
+    //             teamNumber: 0,
+    //             teamName: "Red Alliance",
+    //             color: "rgb(255,0,0)",
+    //             hidden: false,
+    //             autonScore: sumParam(red, "auton_total_score_avg"),
+    //             gamesPlayed: sumParam(red, "nbr_games"),
+    //             gameScore: sumParam(red, "total_game_score_avg"),
+    //             teleopScore: sumParam(red, "teleop_total_score_avg"),
+    //             endgameScore: sumParams(red, ["endgame_park_avg", 2], ["endgame_shallow_climb_avg", 6], ["endgame_deep_climb_avg", 12]),
+    //             rank: sumParam(red, "api_rank"),
+    //             opr: sumParam(red, "api_opr"),
+    //             dpr: sumParam(red, "api_dpr")
+    //         },
+    //         {
+    //             teamNumber: 1,
+    //             teamName: "Blue Alliance",
+    //             color: "rgb(0,0,255)",
+    //             hidden: false,
+    //             autonScore: sumParam(blue, "auton_total_score_avg"),
+    //             gamesPlayed: sumParam(blue, "nbr_games"),
+    //             gameScore: sumParam(blue, "total_game_score_avg"),
+    //             teleopScore: sumParam(blue, "teleop_total_score_avg"),
+    //             endgameScore: sumParams(blue, ["endgame_park_avg", 2], ["endgame_shallow_climb_avg", 6], ["endgame_deep_climb_avg", 12]),
+    //             rank: sumParam(blue, "api_rank"),
+    //             opr: sumParam(blue, "api_opr"),
+    //             dpr: sumParam(blue, "api_dpr")
+    //         }
+    //     ],
+    //     false
+    // )
+    // consoleLog("CONF:")
+    // consoleLog(config)
+    // chart = new Chart(ctx,
+    //     config
+    // )
 }
