3a7783c1e932a10bd9f3cdf2245d896d3337620b
zyxwzyxw <akahn5@outlook.com>
2026-03-18 14:03:10 -0400

    auton path

--- client/static/scripts/team-details.js
+++ client/static/scripts/team-details.js
@@ -190,17 +190,29 @@ async function main() {
         const ctx = canvas.getContext("2d")
         const selectedOption = select.children[select.selectedIndex]
         const allianceColor = selectedOption.getAttribute("alliance_color") || "B"
-        const loadpath = decodeURIComponent(
-            selectedOption.getAttribute("auton_path") || "",
-        )
+        // const loadpath = decodeURIComponent(
+        //     selectedOption.getAttribute("auton_path") || "",
+        // )
 
+const loadpath = selectedOption.getAttribute("auton_path");
+
+let fieldpaths = [];
+
+if (loadpath) {
+    try {
+        fieldpaths = JSON.parse(decodeURIComponent(loadpath));
+    } catch (err) {
+        console.error("Failed to parse auton_path:", err);
+    }
+}
 
         // if (canvasCTX.reset) {
         //     canvasCTX.reset()
         // } else {
         //     canvasCTX.clearRect(0, 0, canvas.width, canvas.height)
         // }
-   let fieldpaths = loadpath
+   //let fieldpaths = loadpath
+
 //   let fieldpaths = [
 //             {"type":"path","points":[{"x":509.54004,"y":208.6875},{"x":509.54004,"y":208.6875},{"x":514.73535,"y":210.34277},{"x":520.46094,"y":212.18848},{"x":528.3291,"y":214.66211},{"x":535.86914,"y":217.04297},{"x":542.5176,"y":219.28418},{"x":548.7363,"y":221.45508},{"x":554.7617,"y":223.59277},{"x":561.1699,"y":225.75684},{"x":569.0664,"y":228.25098},{"x":576.7969,"y":230.49902},{"x":584.7383,"y":232.82324},{"x":592.3887,"y":234.98047},{"x":599.22754,"y":237.0918},{"x":604.38184,"y":238.87793},{"x":609.52637,"y":240.71777},{"x":615.292,"y":243.09277},{"x":620.3506,"y":245.16016},{"x":625.02246,"y":247.19238},{"x":629.6797,"y":249.27832},{"x":633.86035,"y":251.02637},{"x":636.8838,"y":252.29199},{"x":639.7207,"y":253.46582},{"x":642.0254,"y":254.46973},{"x":643.8828,"y":255.33496},{"x":645.68066,"y":256.2256},{"x":646.7998,"y":256.75},{"x":647.83203,"y":257.26562},{"x":649.01074,"y":257.95508},{"x":650.0781,"y":258.44727},{"x":651.0625,"y":258.9248},{"x":651.6006,"y":259.0996}]},
 //             {"type":"labeled","points":[{"x":664.0,"y":270.9004}],"label":"shoot"},
@@ -243,19 +255,66 @@ function draw(image) {
         else drawLabeledStroke(stroke);
     });
 }
+function drawArrowhead(x, y, angle, size = 10) {
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.rotate(angle);
 
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(-size, size / 2);
+    ctx.lineTo(-size, -size / 2);
+    ctx.closePath();
+    ctx.fillStyle = "black";
+    ctx.fill();
+
+    ctx.restore();
+}
+
+// function drawPathStroke(stroke) {
+//     const pts = stroke.points;
+//     ctx.strokeStyle = "black";
+//     ctx.lineWidth = 6;
+
+//     for (let i = 1; i < pts.length; i++) {
+//         ctx.beginPath();
+//         ctx.moveTo(pts[i - 1].x * scale, pts[i - 1].y * scale);
+//         ctx.lineTo(pts[i].x * scale, pts[i].y * scale);
+//         ctx.stroke();
+//     }
+// }
 function drawPathStroke(stroke) {
     const pts = stroke.points;
     ctx.strokeStyle = "black";
     ctx.lineWidth = 6;
 
+    const arrowSpacing = 12; // draw arrow every 12 points
+
     for (let i = 1; i < pts.length; i++) {
+        const p1 = pts[i - 1];
+        const p2 = pts[i];
+
+        // Draw the line segment
         ctx.beginPath();
-        ctx.moveTo(pts[i - 1].x * scale, pts[i - 1].y * scale);
-        ctx.lineTo(pts[i].x * scale, pts[i].y * scale);
+        ctx.moveTo(p1.x * scale, p1.y * scale);
+        ctx.lineTo(p2.x * scale, p2.y * scale);
         ctx.stroke();
+
+        // Draw arrow every N segments
+        if (i % arrowSpacing === 0) {
+            const dx = (p2.x - p1.x);
+            const dy = (p2.y - p1.y);
+            const angle = Math.atan2(dy, dx);
+
+            // Arrow at the midpoint of the segment
+            const midX = (p1.x + p2.x) / 2 * scale;
+            const midY = (p1.y + p2.y) / 2 * scale;
+
+            drawArrowhead(midX, midY, angle);
+        }
     }
 }
+
 
 function drawLabeledStroke(stroke) {
     const { x, y } = stroke.points[0];
