945ff4fa7b64399b5e44f3913ddecc3b8c0c2084
alexfinger21 <cdx9933@gmail.com>
2026-03-18 12:28:23 -0400

    opr update

--- server/getRanks.js
+++ server/getRanks.js
@@ -10,265 +10,290 @@ dotenv.config()
 const auth = process.env.TBA_AUTH
 const authbase64 = Buffer.from(auth, 'utf8').toString('base64')
 const optionsOPRS = {
-  'method': 'GET',
-  //'url': 'https://frc-api.firstinspires.org/v3.0/' + gameConstants.YEAR + '/rankings/'+gameConstants.COMP,
-  'url': 'https://www.thebluealliance.com/api/v3/event/' + gameConstants.YEAR + (gameConstants.COMP == "ohwr" ? "ohwar" : gameConstants.COMP) + '/oprs',
-  'headers': {
-    'X-TBA-Auth-Key': auth,
-    'If-Modified-Since': ''
-  }
+    'method': 'GET',
+    //'url': 'https://frc-api.firstinspires.org/v3.0/' + gameConstants.YEAR + '/rankings/'+gameConstants.COMP,
+    'url': 'https://www.thebluealliance.com/api/v3/event/' + gameConstants.YEAR + (gameConstants.COMP == "ohwr" ? "ohwar" : gameConstants.COMP) + '/oprs',
+    'headers': {
+        'X-TBA-Auth-Key': auth,
+        'If-Modified-Since': ''
+    }
 }
 function printMessage(title, msg) {
-  consoleLog("------ " + title + " ------")
-  consoleLog(msg)
-  consoleLog("------End Message------")
+    consoleLog("------ " + title + " ------")
+    consoleLog(msg)
+    consoleLog("------End Message------")
 }
 
 printMessage('Base64', authbase64)
 // printMessage('Options', options)
 
 let showObj = function () {
-  consoleLog("Shape")
-  consoleLog(teamData.headers)
-  for (let prop in teamData) {
-    consoleLog(1)
-    consoleLog(prop)
-    //   consoleLog(prop)
-    //   consoleLog(teamData[prop])
-  }
+    consoleLog("Shape")
+    consoleLog(teamData.headers)
+    for (let prop in teamData) {
+        consoleLog(1)
+        consoleLog(prop)
+        //   consoleLog(prop)
+        //   consoleLog(teamData[prop])
+    }
 }
 function calculateOpr(matches, scoreProperty, weightProperty) {
-  const alliances = new Array(matches.length * 2)
-  const teams = new Set()
-  const scores = new Array(alliances.length)
+    const alliances = new Array(matches.length * 2)
+    const teams = new Set()
+    const scores = new Array(alliances.length)
+    const totalWeights = {}
 
 
-  let m_idx = 0;
-  for (const match of matches) {
-    [...match.red.teams, ...match.blue.teams].forEach(t => {
-      teams.add(t)
-    })
+    let m_idx = 0;
+    for (const match of matches) {
+        [...match.red.teams, ...match.blue.teams].forEach(t => {
+            teams.add(t)
+        })
 
-    scores[m_idx] = [match.red[scoreProperty]]
-    scores[m_idx + 1] = [match.blue[scoreProperty]]
+        scores[m_idx] = [Math.pow(match.red[scoreProperty], 2)] 
+        scores[m_idx + 1] = [Math.pow(match.blue[scoreProperty], 2)]
 
-    m_idx += 2
-  }
-
-  const teamArr = Array.from(teams)
-  const teamIdx = {}
-
-  for (let i = 0; i<teams.size; ++i) {
-    teamIdx[teamArr[i]] = i
-  }
-
-  //console.log(teamIdx)
-
-  for (let idx = 0; idx<alliances.length; ++idx) {
-    alliances[idx] = new Array(teamArr.length).fill(0)
-  }
-
-  m_idx = 0;
-  for (const match of matches) {
-    let t_idx = 0
-    for (const t of match.red.teams) {
-      //console.log(t, alliances[m_idx])
-      alliances[m_idx][teamIdx[t]] = match.red[weightProperty][t_idx] 
-      ++t_idx
+        m_idx += 2
     }
 
-    t_idx = 0
-    for (const t of match.blue.teams) {
-      alliances[m_idx+1][teamIdx[t]] = match.blue[weightProperty][t_idx] 
-      ++t_idx
+    const teamArr = Array.from(teams)
+    const teamIdx = {}
+
+    for (let i = 0; i<teams.size; ++i) {
+        teamIdx[teamArr[i]] = i
     }
 
-    m_idx += 2
-  }
+    //console.log(teamIdx)
 
-  const AMatrix = new Matrix(alliances)
-  const bMatrix = new Matrix(scores)
+    for (let idx = 0; idx<alliances.length; ++idx) {
+        alliances[idx] = new Array(teamArr.length).fill(0)
+    }
 
-  const oprMatrix = solve(AMatrix, bMatrix)
-  const oprMap = {} 
+    m_idx = 0;
+    for (const match of matches) {
+        let t_idx = 0
+        for (const t of match.red.teams) {
+            //console.log(t, alliances[m_idx])
+            if (!totalWeights[t]) {
+                totalWeights[t] = [match.red[weightProperty][t_idx], 1]
+            } else {
+                totalWeights[t][0] += match.red[weightProperty][t_idx]
+                ++totalWeights[t][1] 
+            }
 
-  for (let i = 0; i<teamArr.length; ++i) {
-    oprMap[teamArr[i]] = oprMatrix.get(i, 0)
-  }
+            alliances[m_idx][teamIdx[t]] = match.red[weightProperty][t_idx] 
+            ++t_idx
+        }
 
-  return oprMap
+        t_idx = 0
+        for (const t of match.blue.teams) {
+            if (!totalWeights[t]) {
+                totalWeights[t] = [match.blue[weightProperty][t_idx], 1]
+            } else {
+                totalWeights[t][0] += match.blue[weightProperty][t_idx]
+                ++totalWeights[t][1] 
+            }
+
+            alliances[m_idx+1][teamIdx[t]] = match.blue[weightProperty][t_idx] 
+            ++t_idx
+        }
+
+        m_idx += 2
+    }
+
+    const AMatrix = new Matrix(alliances)
+    const bMatrix = new Matrix(scores)
+
+    const oprMatrix = solve(AMatrix, bMatrix)
+    const oprMap = {} 
+
+    for (let i = 0; i<teamArr.length; ++i) {
+        const cumWeight = totalWeights[teamArr[i]][0] / totalWeights[teamArr[i]][1]
+
+        oprMap[teamArr[i]] = oprMatrix.get(i, 0) * cumWeight
+    }
+
+    return oprMap
 }
 
 function calculateDpr(matches, opr, scoreProperty, weightProperty) {
-  const alliances = new Array(matches.length * 2)
-  const teams = new Set()
-  const scores = new Array(alliances.length)
+    const alliances = new Array(matches.length * 2)
+    const teams = new Set()
+    const scores = new Array(alliances.length)
 
 
-  let m_idx = 0;
-  for (const match of matches) {
-    [...match.red.teams, ...match.blue.teams].forEach(t => {
-      teams.add(t)
-    })
+    let m_idx = 0;
+    for (const match of matches) {
+        [...match.red.teams, ...match.blue.teams].forEach(t => {
+            teams.add(t)
+        })
 
-    scores[m_idx] = [match.red[scoreProperty] - match.red.teams.reduce(
-      (p, a) => {
-        a + opr[p]
-      }, 0)]
+        scores[m_idx] = [match.red[scoreProperty] - match.red.teams.reduce(
+            (p, a) => {
+                a + opr[p]
+            }, 0)]
 
-    scores[m_idx] = [match.blue[scoreProperty] - match.blue.teams.reduce(
-      (p, a) => {
-        a + opr[p]
-      }, 0)]
+        scores[m_idx] = [match.blue[scoreProperty] - match.blue.teams.reduce(
+            (p, a) => {
+                a + opr[p]
+            }, 0)]
 
-    m_idx += 2
-  }
-
-  const teamArr = Array.from(teams)
-  const teamIdx = {}
-
-  for (let i = 0; i<teams.size; ++i) {
-    teamIdx[teamArr[i]] = i
-  }
-
-  for (let idx = 0; idx<alliances.length; ++idx) {
-    alliances[idx] = new Array(teamArr.length).fill(0)
-  }
-
-  m_idx = 0;
-  for (const match of matches) {
-    let t_idx = 0
-    for (const t of match.red.teams) {
-      console.log(t, alliances[m_idx])
-      alliances[m_idx][teamIdx[t]] = match.red[weightProperty][t_idx] 
-      ++t_idx
+        m_idx += 2
     }
 
-    t_idx = 0
-    for (const t of match.blue.teams) {
-      alliances[m_idx+1][teamIdx[t]] = match.blue[weightProperty][t_idx] 
-      ++t_idx
+    const teamArr = Array.from(teams)
+    const teamIdx = {}
+
+    for (let i = 0; i<teams.size; ++i) {
+        teamIdx[teamArr[i]] = i
     }
 
-    m_idx += 2
-  }
+    for (let idx = 0; idx<alliances.length; ++idx) {
+        alliances[idx] = new Array(teamArr.length).fill(0)
+    }
 
-  const AMatrix = new Matrix(alliances)
-  const bMatrix = new Matrix(scores)
+    m_idx = 0;
+    for (const match of matches) {
+        let t_idx = 0
+        for (const t of match.red.teams) {
+            console.log(t, alliances[m_idx])
+            alliances[m_idx][teamIdx[t]] = match.red[weightProperty][t_idx] 
+            ++t_idx
+        }
 
-  const oprMatrix = solve(AMatrix, bMatrix)
-  const oprMap = {} 
+        t_idx = 0
+        for (const t of match.blue.teams) {
+            alliances[m_idx+1][teamIdx[t]] = match.blue[weightProperty][t_idx] 
+            ++t_idx
+        }
 
-  for (let i = 0; i<teamArr.length; ++i) {
-    oprMap[teamArr[i]] = oprMatrix.get(i, 0)
-  }
+        m_idx += 2
+    }
 
-  return oprMap
+    const AMatrix = new Matrix(alliances)
+    const bMatrix = new Matrix(scores)
+
+    const oprMatrix = solve(AMatrix, bMatrix)
+    const oprMap = {} 
+
+    for (let i = 0; i<teamArr.length; ++i) {
+        oprMap[teamArr[i]] = oprMatrix.get(i, 0)
+    }
+
+    return oprMap
 }
 
 function returnAPIRankings() {
-  return new Promise((resolve, reject) => {
-    if (gameConstants.COMP == "test") {
-      resolve({})
-      return
-    }
-    request(optionsOPRS, function(error, response) {
-      if (error) throw new Error(error)
-      printMessage("Status Code", response.statusCode)
-      //consoleLog(response.body)
-      const oprData = JSON.parse(response.body)
-
-      //consoleLog(oprData)
-
-      for (const [rankings, _] of Object.entries(oprData)) {
-        for (const [i, val] of Object.entries(oprData[rankings])) {
-          oprData[rankings][i.substring(3)] = oprData[rankings][i] //remove the first 3 chars in front 'frc'
-          delete oprData[rankings][i]
+    return new Promise((resolve, reject) => {
+        if (gameConstants.COMP == "test") {
+            resolve({})
+            return
         }
+        request(optionsOPRS, function(error, response) {
+            if (error) throw new Error(error)
+            printMessage("Status Code", response.statusCode)
+            //consoleLog(response.body)
+            const oprData = JSON.parse(response.body)
 
-      }
+            //consoleLog(oprData)
 
-      request(optionsRankings, function(error, response) {
-        const rankingsData = JSON.parse(response.body).rankings
-        //consoleLog(rankingsData)
-        const combinedTeamData = {}
-        if (rankingsData) {
+            for (const [rankings, _] of Object.entries(oprData)) {
+                for (const [i, val] of Object.entries(oprData[rankings])) {
+                    oprData[rankings][i.substring(3)] = oprData[rankings][i] //remove the first 3 chars in front 'frc'
+                    delete oprData[rankings][i]
+                }
 
-          for (let i = 0; i<rankingsData.length; i++) {
-            combinedTeamData[rankingsData[i].team_key.substring(3)] = rankingsData[i]
-            combinedTeamData[rankingsData[i].team_key.substring(3)].opr = oprData?.["oprs"]?.[rankingsData[i].team_key.substring(3)]
-            combinedTeamData[rankingsData[i].team_key.substring(3)].dpr = oprData?.["dprs"]?.[rankingsData[i].team_key.substring(3)]
-          }
+            }
 
-          //consoleLog(database.writeAPIRankings(combinedTeamData))
-          //consoleLog(combinedTeamData)    
-          if (Object.keys(combinedTeamData).length) { 
-            database.query(database.deleteAPIRankings(), (err, res) => {
-              consoleLog(err)
-              //consoleLog(res)
-              database.query(database.writeAPIRankings(combinedTeamData), (err, res) => {
-                consoleLog(err)
-                //consoleLog(res)
-              })
+            request(optionsRankings, function(error, response) {
+                const rankingsData = JSON.parse(response.body).rankings
+                //consoleLog(rankingsData)
+                const combinedTeamData = {}
+                if (rankingsData) {
+
+                    for (let i = 0; i<rankingsData.length; i++) {
+                        combinedTeamData[rankingsData[i].team_key.substring(3)] = rankingsData[i]
+                        combinedTeamData[rankingsData[i].team_key.substring(3)].opr = oprData?.["oprs"]?.[rankingsData[i].team_key.substring(3)]
+                        combinedTeamData[rankingsData[i].team_key.substring(3)].dpr = oprData?.["dprs"]?.[rankingsData[i].team_key.substring(3)]
+                    }
+
+                    //consoleLog(database.writeAPIRankings(combinedTeamData))
+                    //consoleLog(combinedTeamData)    
+                    if (Object.keys(combinedTeamData).length) { 
+                        database.query(database.deleteAPIRankings(), (err, res) => {
+                            consoleLog(err)
+                            //consoleLog(res)
+                            database.query(database.writeAPIRankings(combinedTeamData), (err, res) => {
+                                consoleLog(err)
+                                //consoleLog(res)
+                            })
+                        })
+                    }
+
+
+                    //consoleLog(combinedTeamData)
+                }
+                resolve(combinedTeamData)
             })
-          }
 
 
-          //consoleLog(combinedTeamData)
-        }
-        resolve(combinedTeamData)
-      })
-
-
-      //consoleLog(teamData)
-      //printMessage('Type of Data', typeof teamData)
-      // teamData.teams.forEach((team) => {
-      //   printMessage('Team Info', team)
-      // }
-      // showObj()
-      // printMessage('Length of Teams array', team_data.teams.teamNumber)
-      //printMessage('Data', teamData)
-      //consoleLog(teamData.Rankings[0].teamNumber)
+            //consoleLog(teamData)
+            //printMessage('Type of Data', typeof teamData)
+            // teamData.teams.forEach((team) => {
+                //   printMessage('Team Info', team)
+                // }
+                // showObj()
+                // printMessage('Length of Teams array', team_data.teams.teamNumber)
+                //printMessage('Data', teamData)
+                //consoleLog(teamData.Rankings[0].teamNumber)
+            })
     })
-  })
 }
 
 async function writeBlueAllianceData(matchData) {
-  const startingIndex = await getLatestMatchWithData()
-  const [err, res] = database.query(database.updateGameDetails(matchData, startingIndex))
-  if(err) {
-    consoleLog("Error writing blue alliance data: ", err)
-  }
-  else {
-    consoleLog("Saved blue alliance data successfully")
-  }
+    const startingIndex = await getLatestMatchWithData()
+    const [err, res] = database.query(database.updateGameDetails(matchData, startingIndex))
+    if(err) {
+        consoleLog("Error writing blue alliance data: ", err)
+    }
+    else {
+        consoleLog("Saved blue alliance data successfully")
+    }
 }
 
 
 async function syncServer() {
-  const data = await getMatchData()
-  console.dir(data, { depth: null, colors: true });
-  //const rankings = await returnAPIRankings()
-  const teleopOpr = calculateOpr(data, "teleopFuel", "teleopWeights")
- // const autoOpr = calculateOpr(data, "autonFuel", "autonWeights")
-  // TODO: implement DPR and figure out how to use the defensive weights
-//  const dpr = calculateDpr(data, teleopOpr, "teleopFuel", "defenseWeights") 
-  //write to server 
-  console.log("teleop opr", teleopOpr)
-  /*
-  database.query(database.deleteAPICalc(), (err, res) => {
-    if(err) {
-      console.log(err)
-      return
-    }
-    else {
-      database.query(database.writeAPICalc(teleopOpr, autoOpr), (err, res => {
-        if(err) {
-          console.log(err);
-        }
-      }))
-    }
-  }) */
+    const data = await getMatchData()
+    console.dir(data, { depth: null, colors: true })
+
+    //const rankings = await returnAPIRankings()
+    const teleopOpr = calculateOpr(data, "teleopFuel", "teleopWeights")
+    const oprValues = Object.entries(teleopOpr).sort((a, b) => 
+        b[1] - a[1]
+    ).map(val => {
+        return [val[0], val[1] > 0 
+            ? Math.sqrt(val[1]) 
+            : -Math.sqrt(Math.abs(val[1]))]
+    })
+    // const autoOpr = calculateOpr(data, "autonFuel", "autonWeights")
+    // TODO: implement DPR and figure out how to use the defensive weights
+    //  const dpr = calculateDpr(data, teleopOpr, "teleopFuel", "defenseWeights") 
+    //write to server 
+    console.log("teleop opr", oprValues)
+    /*
+        database.query(database.deleteAPICalc(), (err, res) => {
+            if(err) {
+                console.log(err)
+                return
+            }
+            else {
+                database.query(database.writeAPICalc(teleopOpr, autoOpr), (err, res => {
+                    if(err) {
+                        console.log(err);
+                    }
+                }))
+            }
+        }) */
 }
 
 syncServer()
